aa180c68a88df4be45366a40529a928fe26a1cfe
v1nnyc <cannallav019@yahoo.com>
2018-04-18 08:25:50 -0700

    test

--- threads/KThread.java
+++ threads/KThread.java
@@ -312,7 +312,7 @@ public class KThread {
 			Machine.interrupt().disable();
 			//sleep();
 			currentThread.status = statusReady;
-			this.status = statusRunning;
+			runNextThread();
 
 			while(this.status != statusFinished){
 				System.out.println(this.toString() + "is still running!");
