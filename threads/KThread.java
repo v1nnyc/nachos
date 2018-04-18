cb302d151245867b16dbfeb79ff26386d5769654
v1nnyc <cannallav019@yahoo.com>
2018-04-18 09:22:09 -0700

    test

--- threads/KThread.java
+++ threads/KThread.java
@@ -297,7 +297,7 @@ public class KThread {
 		this.setCaller(currentThread);
 
 		Machine.interrupt().disable();
-		currentThread.yield();
+		currentThread.sleep();
 		Machine.interrupt().enable();
 
 		return;
